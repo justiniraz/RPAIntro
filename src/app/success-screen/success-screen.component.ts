65168e714b2d52c9e99aa881449cdb4062bd9a97
Justin Irazabal <justin@justins-mbp.lan>
2018-12-19 13:37:31 -0500

    success-screen finished

--- src/app/success-screen/success-screen.component.ts
+++ src/app/success-screen/success-screen.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-success-screen',
   templateUrl: './success-screen.component.html',
   styleUrls: ['./success-screen.component.scss']
 })
-export class SuccessScreenComponent implements OnInit {
+export class SuccessScreenComponent {
+  now: number;
+  today = new Date();
+  minutes = '';
 
-  constructor() { }
+  constructor() {
+    setInterval(() => {
+      this.now = Date.now();
+    }, 1);
 
-  ngOnInit() {
+    this.minutes = formatDate(this.today, 'mm', 'en-US', '-0500');
   }
-
 }
+
+
